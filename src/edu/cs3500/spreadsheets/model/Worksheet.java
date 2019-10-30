15f35b5b90576d242020b42dd212063f483bca31
sethfriman <56849841+sethfriman@users.noreply.github.com>
2019-10-30 18:36:57 -0400

    references

--- src/edu/cs3500/spreadsheets/model/Worksheet.java
+++ src/edu/cs3500/spreadsheets/model/Worksheet.java
@@ -5,6 +5,7 @@ import edu.cs3500.spreadsheets.sexp.SList;
 import edu.cs3500.spreadsheets.sexp.SSymbol;
 import edu.cs3500.spreadsheets.sexp.Sexp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -34,13 +35,80 @@ public class Worksheet {
     else if (key.contains(":")) {
       String left = key.substring(0, key.indexOf(":"));
       String right = key.substring(key.indexOf(":") + 1);
-      if (this.sheet.containsKey(left) && this.sheet.containsKey(right)) {
-        List<Cell> references = this.getAllReferences(left, right);
+      if (!(this.isValidName(left) && this.isValidName(right))) {
+        throw new IllegalArgumentException("Invalid Cell Name");
       }
+      else {
+        String leftCol = "";
+        String rightCol = "";
+        int leftRow = 0;
+        int rightRow = 0;
+        for (int i = 0; i < left.length(); i++) {
+          if (Character.isDigit(left.charAt(i))) {
+            leftCol = left.substring(0, i);
+            leftRow = Integer.parseInt(left.substring(i));
+            break;
+          }
+        }
+        for (int i = 0; i < right.length(); i++) {
+          if (Character.isDigit(right.charAt(i))) {
+            rightCol = right.substring(0, i);
+            rightRow = Integer.parseInt(right.substring(i));
+            break;
+          }
+        }
+        Coord topLeft;
+        Coord bottomRight;
+        if (left.toUpperCase().compareTo(right.toUpperCase()) <= 0) {
+          topLeft = new Coord(Coord.colNameToIndex(leftCol), leftRow);
+          bottomRight = new Coord(Coord.colNameToIndex(rightCol), rightRow);
+        }
+        else {
+          bottomRight = new Coord(Coord.colNameToIndex(leftCol), leftRow);
+          topLeft = new Coord(Coord.colNameToIndex(rightCol), rightRow);
+        }
+        List<String> references = this.getAllReferences(topLeft, bottomRight);
+        return null; // this needs to be in function object so that function
+        // can be applied to all things inside
+      }
+    }
+    else if (this.isValidName(key)) {
+      return null; // this needs to be put into function object so it can default
+    }
+    else {
+      throw new IllegalArgumentException("Can not Evaluate Symbol");
     }
   }
 
-  public List<Cell> getAllReferences() {
-
+  private boolean isValidName(String cell) {
+    int index = -1;
+    for (int i = 0; i < cell.length(); i++) {
+      if (Character.isDigit(cell.charAt(i))) {
+        index = i;
+        try {
+          String celCol = cell.substring(0, index);
+          int celRow = Integer.parseInt(cell.substring(index));
+          return ((!celCol.equals(""))
+              && (celCol.matches("^[a-zA-Z]*$")));
+        }
+        catch (Exception e) {
+          return false;
+        }
+      }
+    }
+    return false;
   }
+
+  public List<String> getAllReferences(Coord tl, Coord br) {
+    List<String> references = new ArrayList<>();
+    for (int i = tl.col; i <= br.col; i++) {
+      for (int j = tl.row; j <= br.row; j++) {
+        Coord temp = new Coord(i, j);
+        references.add(temp.toString());
+      }
+    }
+    return references;
+  }
+
+
 }
