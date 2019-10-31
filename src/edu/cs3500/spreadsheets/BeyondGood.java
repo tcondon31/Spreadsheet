8166a82fc9c7451f29ed3254af449ca00ed0660a
sethfriman <56849841+sethfriman@users.noreply.github.com>
2019-10-31 16:21:03 -0400

    lots of stuff. idk

--- src/edu/cs3500/spreadsheets/BeyondGood.java
+++ src/edu/cs3500/spreadsheets/BeyondGood.java
@@ -1,5 +1,14 @@
 package edu.cs3500.spreadsheets;
 
+import edu.cs3500.spreadsheets.model.Worksheet;
+import edu.cs3500.spreadsheets.model.WorksheetBuilderClass;
+import edu.cs3500.spreadsheets.model.WorksheetReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.StringReader;
+
 /**
  * The main class for our program.
  */
@@ -10,7 +19,41 @@ public class BeyondGood {
    */
   public static void main(String[] args) {
 
+    File f;
+    Worksheet w;
+    WorksheetReader.WorksheetBuilder wbc;
+    Readable r;
 
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    System.out.println(args[2]);
+    System.out.println(args[3]);
+
+    if (args[0].equals("-in")) {
+
+        f = new File(args[1]);
+        w = new Worksheet();
+        wbc = new WorksheetBuilderClass(w);
+      try {
+        r = new FileReader(f);
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        r = null;
+      }
+      WorksheetReader.read(wbc, r);
+
+
+    }
+    else {
+      throw new IllegalArgumentException("Invalid Command Line");
+    }
+
+    if (args[2].equals("-eval")) {
+      System.out.println(w.evaluateCell(w.getCellAt(args[3])));
+    }
+    else {
+      throw new IllegalArgumentException("Invalid Command Line");
+    }
 
     /*
       TODO: For now, look in the args array to obtain a filename and a cell name,
